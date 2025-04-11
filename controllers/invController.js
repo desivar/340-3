6aacaf22006fce8d13b93146ccfd16c4e5494c3f
desivar <jillian_desire@yahoo.es>
2025-04-11 08:09:27 -0600

    fix error

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,6 +1,5 @@
-const invModel = require('../models/inventory-model')
-const reviewModel = require('../models/review-model')
-const utilities = require('../utilities/')
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities/")
 
 const invCont = {}
 
@@ -8,16 +7,15 @@ const invCont = {}
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-    const classification_id = req.params.classificationId
-    const data = await invModel.getInventoryByClassificationId(classification_id)
-    const grid = await utilities.buildClassificationGrid(data)
-    let nav = await utilities.getNav()
-    const className = data[0].classification_name
-    res.render('inventory/classification', {
-        title: className + ' vehicles',
-        nav,
-        grid,
-        errors: null,
-    })
+  const classification_id = req.params.classificationId
+  const data = await invModel.getInventoryByClassificationId(classification_id)
+  const grid = await utilities.buildClassificationGrid(data)
+  let nav = await utilities.getNav()
+  const className = data[0].classification_name
+  res.render("./inventory/classification", {
+    title: className + " vehicles",
+    nav,
+    grid,
+  })
 }
 module.exports = invCont
