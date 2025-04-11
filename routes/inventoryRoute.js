d29d6ae707e6e0a5441ced837c779b814e3d963b
desivar <jillian_desire@yahoo.es>
2025-04-11 08:27:52 -0600

    fix ed errors

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,7 +1,4 @@
-// Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const invController = require("../controllers/invController")
+
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const router = new express.Router() 
-const invController = require("../controllers/invController")
+const invController = require("./controllers/invController")
 
 /* ***********************
  * Routes
