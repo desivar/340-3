574986503482bd69829213dcb319640f5852bc5c
desivar <jillian_desire@yahoo.es>
2025-04-11 08:05:26 -0600

    updated invControl

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,5 +1,6 @@
-const invModel = require("../models/inventory-model")
-const utilities = require("../utilities/")
+const invModel = require('../models/inventory-model')
+const reviewModel = require('../models/review-model')
+const utilities = require('../utilities/')
 
 const invCont = {}
 
@@ -7,15 +8,16 @@ const invCont = {}
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  })
+    const classification_id = req.params.classificationId
+    const data = await invModel.getInventoryByClassificationId(classification_id)
+    const grid = await utilities.buildClassificationGrid(data)
+    let nav = await utilities.getNav()
+    const className = data[0].classification_name
+    res.render('inventory/classification', {
+        title: className + ' vehicles',
+        nav,
+        grid,
+        errors: null,
+    })
 }
 module.exports = invCont
