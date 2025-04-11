80f2ef73a6edf51d4ca8f378478c83e417f27364
desivar <jillian_desire@yahoo.es>
2025-04-11 10:27:18 -0600

    set the view ejs

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
-/* ******************************************
- * This server.js file is the primary file of the
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
 const express = require("express");
 const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
-// Add the inventory route import.
 const inventoryRoute = require("./routes/inventoryRoute");
+
+// Set the view engine to EJS
+app.set("view engine", "ejs");
 
 /* ***********************
  * Routes
  *************************/
 app.use(static);
-
-// Add the inventory routes
 app.use("/inv", inventoryRoute);
 
-// Add the root route - Corrected to render index.ejs
 app.get("/", (req, res) => {
-  res.render("index"); // Render the index.ejs file
+    res.render("index"); // Now Express knows to use EJS
 });
 
 /* ***********************
@@ -36,5 +28,5 @@ const host = process.env.HOST;
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`);
+    console.log(`app listening on <span class="math-inline">\{host\}\:</span>{port}`);
 });
