d690a62a5c6dfdd56a4c3dc58ba2296f93600623
desivar <jillian_desire@yahoo.es>
2025-04-11 08:40:35 -0600

    fixed errors

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,4 +1,7 @@
-
+// Needed Resources
+const express = require("express");
+const router = express.Router();
+const invController = require("../controllers/invController");
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
